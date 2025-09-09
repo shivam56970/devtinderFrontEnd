a082c874e7e2127b92ed7a3d255f9c492f4a5816
shivam56970 <shivamtej123456789@gmail.com>
2025-09-09 05:42:23 +0530

    Merged remote changes from origin/main

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export const BASE_URL =
 location.hostname === "localhost" ? "http://localhost:3000/": "/api/";
-=======
-export const BASE_URL = "/api/";
->>>>>>> 310ee90c62814f70db5f5d39b87b228d17ac4082
